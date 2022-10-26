5b4d2f5a34781c3c261e5e9af6a85cd4b46af9ad
Your Name <rawipornsr19@gamil.com>
2022-10-26 21:27:13 +0700

    ก

---
+++ StudentsKMUTNB/test.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request,redirect,url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String, Date
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://webadmin:DRRoas64212@10.104.9.239:/hw'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Comments(db.Model):
+    __tablename__ = 'comments'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    comment = Column(String)
+
+@app.route('/')
+def index():
+    result = Comments.query.all()
+    return render_template('index7.html', result=result)
+
+@app.route('/sign')
+def sign():
+    return render_template('sign7.html')
+
+@app.route('/process', methods=['POST'])
+def process():
+    name = request.form['name']
+    comment = request.form['comment']
+    signature = Comments(name=name, comment=comment)
+    db.session.add(signature)
+    db.session.commit()
+    return redirect(url_for('index'))
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=80, debug=True)
